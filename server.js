ce450dfadd6d8b22c034feee8b9191c9e94623bd
hamadafadel <165340456+hamadafadel@users.noreply.github.com>
2026-04-06 11:27:52 +0200

    for health check

--- server.js
+++ server.js
@@ -67,6 +67,10 @@ app.get('/api/messages/:sessionId', async (req, res) => {
 });
 
 // 🟢 تشغيل السيرفر
+app.get('/health', (req, res) => {
+  res.status(200).json({ ok: true });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
