61fc54c183500ce5b829def01c988568936ea4b8
hamadafadel <165340456+hamadafadel@users.noreply.github.com>
2026-05-05 13:54:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
+const uploadsDir = path.join(__dirname, 'uploads');
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
+app.use('/uploads', express.static(uploadsDir));
 app.get('/debug-files', (req, res) => {
   res.json({
     dir: __dirname,
